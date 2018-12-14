1f4b6108784f877066c581518e10f2d9f8b9b57b
Harsh Kotak <kotak_harsh@hotmail.com>
2018-12-13 19:03:49 -1000

    🚑 adding redux

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import LoggedOut from './app/screens/LoggedOutScreen.js';
 import Login from './app/screens/LoginScreen.js';
 import Registration from './app/screens/RegistrationScreen.js';
 import Home from './app/screens/Home.js'
+import HomeScreen from './app/screens/HomeScreen.js'
 
 import AppNavigator from './navigation/AppNavigator';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
@@ -17,7 +18,7 @@ export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <LoggedOut />
+        <HomeScreen />
       </Provider>
     );
   }
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,2 +1,62 @@
 import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  ActivityIndicator,
+} from 'react-native';
 
+import { connect } from 'react-redux';
+
+// import * as Actions from '../redux/actions/actions.js';
+import { getMyCard } from '../redux/actions/actions.js';
+
+
+// const mapStateToProps = (state) => {
+//   return {
+//     data: state.dataReducer.data
+//   }
+// }
+
+const mapStateToProps = (state) => {
+  console.log("state", state)
+  return {
+    MyCardData: state.MyCardData,
+    myCardCSS: state.myCardCSS
+  }
+}
+
+class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount = () => {
+    this.props.dispatch(getMyCard())
+  }
+
+  render() {
+    console.log('this.props: ', this.props)
+    return (
+      <View>
+        <Text style={styles.text1}>DATA SCREEN</Text>
+      </View>
+    )
+  }
+}
+
+
+export default connect(mapStateToProps)(HomeScreen)
+
+
+
+//~~~~ STYLESHEET ~~~~//
+const styles = StyleSheet.create({
+  text1: {
+    display: 'flex',
+    marginTop: 50,
+    textAlign: 'center',
+    fontSize: 30,
+
+  }
+})
