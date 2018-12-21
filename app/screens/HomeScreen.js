f1193db639850bbcc78cc5064babc827df5553fc
Harsh Kotak <kotak_harsh@hotmail.com>
2018-12-20 18:06:11 -1000

    ✨ web to mobile styling

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -4,24 +4,24 @@ import {
   StyleSheet,
   View,
   Text,
-  ActivityIndicator,
-  ScrollView,
-  Button
+  Button,
+  ImageBackground,
+  Image
 } from 'react-native';
-import { Icon } from 'react-native-elements'
 
+//~~~~ Card Data ~~~~//
+import { Card } from '../components/CardComponent.js';
+import { cardContainer, front, title, back, info, company, name, address, phone, email } from '../components/CardClassing.js';
 
-//aws auth
+//~~~~ AWS Auth ~~~~//
 import { Auth } from 'aws-amplify'
 
-// redux
+//~~~~ Redux ~~~~//
 import { connect } from 'react-redux';
-
-
 import { getMyCard, authenticated } from '../redux/actions/actions.js';
 
 const mapStateToProps = (state) => {
-  console.log("STATE", state)
+  // console.log("STATE", state)
   return {
     myCard: state.myCard,
     // myCardData: state.myCardData,
@@ -53,7 +53,7 @@ class HomeScreen extends Component {
     alert('LOL')
     Auth.signOut()
       .then(data => {
-        console.log("signout data", data)
+        // console.log("signout data", data)
         this.props.navigation.navigate('Auth')
       })
       .catch(err => {
@@ -63,19 +63,94 @@ class HomeScreen extends Component {
 
   render() {
     // console.log("AVAILABLE PROPS: ", this.props);
-    const Data = this.props.myCard.data;
-    console.log("CARD DATA: ", this.props);
+    // console.log("CARD DATA: ", this.props);
+
+    const { myCard } = this.props;
+
+    const { data, css } = myCard;
+
+    console.log("~~~~PROP DATA~~~~");
+    console.log(data);
+    console.log("CSS:", css);
+
+    // ~~~~ STYLESHEET ~~~~//
+    // const newStyles = StyleSheet.create({
+    // })
+
+    // console.log("~~~~STYLE~~~~");
+
+    const styles1 = StyleSheet.create({
+      wrapper: {
+        display: 'flex',
+        backgroundColor: "lightblue",
+        height: '100%',
+        alignItems: 'center',
+      },
+      text1: {
+        marginTop: 50,
+        textAlign: 'center',
+        fontSize: 35,
+      },
+      text2: {
+        textAlign: 'center',
+        fontSize: 30,
+      },
+      cardWrapper: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 30,
+        backgroundColor: 'red',
+        width: '80%',
+        height: '30%',
+        borderRadius: 50,
+      },
+    })
+
+    console.log("STYLES: ", styles1);
 
     return (
-      <View style={styles.wrapper}>
-        <Text style={styles.text1}>Welcome</Text>
-        <Text style={styles.text2}>{Data.name}</Text>
-        <View style={styles.cardWrapper}>
-          <Text>Name: {Data.name}</Text>
-          <Text>Company Name: {Data.company_name}</Text>
-          <Text>Email: {Data.email}</Text>
-          <Text>Phone #: {Data.phone}</Text>
-          <Text>Title: {Data.title}</Text>
+      // <View style={styles.wrapper}>
+      //   <Text style={styles.text1}>Welcome</Text>
+      //   <Text style={styles.text2}>{data.name}</Text>
+
+      //   <View style={styles.cardWrapper}>
+      //     <Text>Name: {data.name}</Text>
+      //     <Text>Company Name: {data.company_name}</Text>
+      //     <Text>Email: {data.email}</Text>
+      //     <Text>Phone #: {data.phone}</Text>
+      //     <Text>Title: {data.title}</Text>
+      //   </View>
+      //   <Button
+      //     onPress={this._logOut}
+      //     title="Log Out"
+      //   />
+
+      // </View>
+
+      <View style={styles1.wrapper}>
+        <Text style={styles1.text1}>Welcome</Text>
+        <Text style={styles1.text2}>{data.name}</Text>
+        <View style={styles1.cardWrapper}>
+          {/* <ImageBackground
+            style={styles.container}
+            resizeMode='cover'
+          // source={{ uri: remote }}
+          > */}
+          <Card
+            cardContainer={cardContainer}
+            front={front}
+            info={info}
+            name={name}
+            title={title}
+            address={address}
+            phone={phone}
+            company_name={company}
+            email={email}
+            data={data}
+            style={css}
+          />
+          {/* </ImageBackground> */}
+
         </View>
         <Button
           onPress={this._logOut}
@@ -83,9 +158,11 @@ class HomeScreen extends Component {
         />
 
       </View>
-
     )
   }
+
+
+
 }
 
 
@@ -94,29 +171,31 @@ export default connect(mapStateToProps)(HomeScreen)
 
 
 //~~~~ STYLESHEET ~~~~//
-const styles = StyleSheet.create({
-  wrapper: {
-    display: 'flex',
-    backgroundColor: "lightblue",
-    height: '100%',
-    alignItems: 'center',
-  },
-  text1: {
-    marginTop: 50,
-    textAlign: 'center',
-    fontSize: 35,
-  },
-  text2: {
-    textAlign: 'center',
-    fontSize: 30,
-  },
-  cardWrapper: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 30,
-    backgroundColor: 'red',
-    width: '80%',
-    height: '30%',
-    borderRadius: 50,
-  }
-})
+
+
+// const styles = StyleSheet.create({
+//   wrapper: {
+//     display: 'flex',
+//     backgroundColor: "lightblue",
+//     height: '100%',
+//     alignItems: 'center',
+//   },
+//   text1: {
+//     marginTop: 50,
+//     textAlign: 'center',
+//     fontSize: 35,
+//   },
+//   text2: {
+//     textAlign: 'center',
+//     fontSize: 30,
+//   },
+//   cardWrapper: {
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     marginTop: 30,
+//     backgroundColor: 'red',
+//     width: '80%',
+//     height: '30%',
+//     borderRadius: 50,
+//   }
+// })
